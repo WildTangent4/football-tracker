ae2dfb8a727524560dcdd5bd04408c22bcc4a87e
Wild Tangent <wildtangent4@gmail.com>
2024-07-24 14:47:30 +0100

    added testing image

--- locate_players.py
+++ locate_players.py
@@ -1,7 +1,13 @@
 import cv2
-
+import numpy as np
+from matplotlib import pyplot as plt
 ### classes i need
 ### worldstate contains pitch and both team objects
 ### team contains all players on a team
 ### player contains player position
 
+img = cv2.imread("test.png")
+plt.imshow(img)
+plt.title("test image")
+plt.xticks([]), plt.yticks([])
+plt.show()
